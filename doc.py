60dfa2067d1b684be10ba86ecc5f1c33a4d4a0e0
erduin17 <42874171+erduin17@users.noreply.github.com>
2018-09-11 09:38:53 -0700

    Add files via upload

---
+++ doc.py
@@ -0,0 +1,7 @@
+# doc.py EM
+""" document how
+	you write your
+	code.
+"""
+print (" hello world ")
+print (" doccument your work ")
---
+++ indent.py
@@ -0,0 +1,5 @@
+#indent.py em
+if (1 > 0):
+	print("greater than")
+if(0<1):
+	print("less than")
