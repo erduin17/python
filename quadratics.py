df3d696d0d2eb97fe4e6d6427f32e46b9956afc9
erduin17 <42874171+erduin17@users.noreply.github.com>
2018-10-03 08:18:57 -0700

    Add files via upload

--- quadratics.py
+++ quadratics.py
@@ -13,3 +13,12 @@ if __name__ == '__main__':
 	c = input ('Enter c: ')
 	roots(float(a),float(b),float(c))
 	
+'''
+terminal output
+
+Enter a: 10
+Enter b: 10
+Enter c: 10
+x1: (-0.4999999999999999+0.8660254037844387j)
+x2: (-0.5000000000000001-0.8660254037844387j)
+'''
